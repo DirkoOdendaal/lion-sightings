e6237f961a0b979bd11bf6b05fed510c4555f6c2
Dirko Odendaal <dirko.odendaal@fixx.it>
2019-02-27 10:36:51 +0100

    Changes to export service

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -17,31 +17,16 @@ export class EmailService {
     constructor(private http: HttpClient) { }
 
     // API: GET /todos
-    public async requestEmail(email_address: string) {
-        // const headers = new Headers();
-        // headers.append('Content-Type', 'text/json');
-        const options = new RequestOptions({
-            headers: new Headers({
-              'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-              'accept-language': 'en-US,en;q=0.9,af;q=0.8',
-              'upgrade-insecure-requests': '1',
-              'X-Requested-By': 'lion-sightings',
-              'X-Requested-At': new Date().toISOString()
+    public requestEmail(email_address: string) {
+          const httpOptions = {
+            headers: new HttpHeaders({
+              'Content-Type':  'application/json'
             })
-          });
+          };
 
-          // const header = new HttpHeaders({
-          //   'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'
-          // });
-          // header.append('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8');
-        // headers.append('authentication', `hello`);
-
-        // const optionsNew = new RequestOptions({ headers: header });
-
-        return await this.http
-            .get(API_URL + '/api/Excel/getEmail/' + email_address)
-            .toPromise()
-            .then(response => Promise.resolve(response), (err: HttpErrorResponse) => this.handleError(err));
+        return this.http
+            .get(API_URL + '/api/Excel/getEmail/' + email_address, httpOptions)
+            .subscribe(response => Promise.resolve(response), (err: HttpErrorResponse) => this.handleError(err));
     }
 
     private handleError(error: HttpErrorResponse) {
